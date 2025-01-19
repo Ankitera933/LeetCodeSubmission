c12536ffbc552da26771f96f8de57242e28856fa
Ankitkumar  patel <131799508+Ankitera933@users.noreply.github.com>
2025-01-19 15:40:57 +0530

    Time: 3 ms (55.43%) | Memory: 43.1 MB (87.44%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,27 +1,34 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int []temp=new int[Math.min(nums1.length,nums2.length)];
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<nums1.length  && j<nums2.length){
-            if(nums1[i]<nums2[j]){
+        HashSet<Integer> set = new HashSet<>();
+        
+        int i = 0, j = 0;
+        
+        // Use two pointers to find common elements
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
                 i++;
-            }
-            else if(nums1[i]>nums2[j]){
+            } else if (nums1[i] > nums2[j]) {
                 j++;
-            }
-            else{
-                if(k==0 || temp[k-1]!=nums1[i]){
-                    temp[k]=nums1[i];
-                    k++;
-                }
+            } else {
+                set.add(nums1[i]);
                 i++;
                 j++;
             }
         }
-        return Arrays.copyOf(temp,k);
+
+        // Convert set to array
+        int[] result = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            result[index++] = num;
+        }
+        
+        return result;
     }
 }
