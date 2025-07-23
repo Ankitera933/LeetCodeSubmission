c9c062675061151bd1b6fbcb9f07058b6022b1e0
Ankitkumar  patel <131799508+Ankitera933@users.noreply.github.com>
2025-07-23 12:41:56 +0530

    Time: 15 ms (38.23%) | Memory: 49 MB (25.57%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,36 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // Step 1: Frequency map
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        // Step 1: Count frequencies
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
 
-        // Step 2: Create buckets where index = frequency
-        List<Integer>[] buckets = new List[nums.length + 1];
-        for (int i = 0; i <= nums.length; i++) {
-            buckets[i] = new ArrayList<>();
-        }
+        // Step 2: Convert map to list of entries and sort by frequency (descending)
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(freqMap.entrySet());
+        entryList.sort((a, b) -> b.getValue() - a.getValue());
 
-        // Step 3: Fill buckets with numbers
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int num = entry.getKey();
-            int freq = entry.getValue();
-            buckets[freq].add(num);
-        }
-
-        // Step 4: Collect top K frequent elements
-        List<Integer> result = new ArrayList<>();
-        for (int i = nums.length; i >= 0 && result.size() < k; i--) {
-            result.addAll(buckets[i]);
-        }
-
-        // Step 5: Convert to array of size k
-        int[] res = new int[k];
+        // Step 3: Extract top k frequent elements
+        int[] result = new int[k];
         for (int i = 0; i < k; i++) {
-            res[i] = result.get(i);
+            result[i] = entryList.get(i).getKey();
         }
 
-        return res;
+        return result;
     }
 }
