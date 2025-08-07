dfde5e08cf209de9dea4b202cc9a346e51ff8827
Ankitkumar  patel <131799508+Ankitera933@users.noreply.github.com>
2025-08-07 23:47:00 +0530

    Time: 3 ms (93.99%) | Memory: 52.8 MB (15.32%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,26 +1,21 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int left = 0;
-        int maxLength = 0;
-
-        for (int right = 0; right < nums.length; right++) {
-            // If we see a zero, reduce the available flips
-            if (nums[right] == 0) {
+        int left=0;
+        int max=0;
+        for(int right=0;right<nums.length;right++){
+            if(nums[right]==0){
                 k--;
-            }
-
-            // If flips used exceed k, shrink window from the left
-            while (k < 0) {
-                if (nums[left] == 0) {
-                    k++; // Reclaim a flip
+                while(k<0){
+                    
+                    if(nums[left]==0){
+                        k++;
+                    }
+                    left++;
                 }
-                left++;
+               
             }
-
-            // Update max length of the valid window
-            maxLength = Math.max(maxLength, right - left + 1);
+             max=Math.max(max,right-left+1);
         }
-
-        return maxLength;
+        return max;
     }
 }
