c910028dea52920f6aefe205bfcb69216e0f18bc
Ankitkumar  patel <131799508+Ankitera933@users.noreply.github.com>
2025-08-26 23:59:48 +0530

    Time: 11 ms (35.17%) | Memory: 49 MB (9.33%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int left=0;
+        int right=0;
+        int count=0;
+        while(left<g.length && right<s.length){
+            if(s[right]>=g[left]){
+                count++;
+                left++;
+                right++;
+            }
+            else if(s[right]<g[left]){
+                right++;
+            }
+            else{
+                left++;
+            }
+        }
+        return count;
+    }
+}
